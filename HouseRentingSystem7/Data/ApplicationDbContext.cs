416630e801e34d00baeb0555138e42ce061ed95e
MVM)7 <mvmanchev07@gmail.com>
2025-08-14 19:25:27 +0300

    Added DB Context

---
+++ HouseRentingSystem7/Data/ApplicationDbContext.cs
@@ -0,0 +1,37 @@
+﻿using HouseRentingSystem7.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HouseRentingSystem7.ApplicationDbContext
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Agent> Agents { get; set; }
+
+        public DbSet<House> Houses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<House>()
+                .HasOne(h => h.Category)
+                .WithMany(c => c.Houses)
+                .HasForeignKey(h => h.CategoryId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.Entity<House>()
+                .HasOne(h => h.Agent)
+                .WithMany()
+                .HasForeignKey(h => h.AgentId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}

fc4869f6cc5cd01b724fabaa610fa439b8d0de21
MVM)7 <mvmanchev07@gmail.com>
2025-08-14 21:46:19 +0300

    Controllers added

---
+++ HouseRentingSystem7/Controllers/AgentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem7.Controllers
+{
+    public class AgentController : Controller
+    {
+        public IActionResult Become()
+        {
+            return View();
+        }
+    }
+}

5b03b35cabc863f39472bf6e98ecd26b034e3563
MVM)7 <mvmanchev07@gmail.com>
2025-08-21 23:08:54 +0300

    AgentService functionallity added.

--- HouseRentingSystem7.Core/Services/AgentService.cs
+++ HouseRentingSystem7.Core/Services/AgentService.cs
@@ -1,5 +1,7 @@
 ﻿using HouseRentingSystem7.Core.Contracts;
+using HouseRentingSystem7.Data.Models;
 using HouseRentingSystem7.Infrastructure.Data.Common;
+using Microsoft.EntityFrameworkCore;
 
 namespace HouseRentingSystem7.Core.Services
 {
@@ -12,9 +14,39 @@ namespace HouseRentingSystem7.Core.Services
             repository = _repository;
         }
 
-        public async Task<bool> ExistsById(string userId)
+        public async Task CreateAsync(string userId, string phoneNumber)
         {
-            
+            var agent = new Agent()
+            {
+                UserId = userId,
+                PhoneNumber = phoneNumber
+            };
+
+
+        }
+
+        public async Task<bool> ExistsByIdAsync(string userId)
+        {
+            var agent = await repository.AllReadOnly<Agent>()
+                .AnyAsync(a => a.UserId == userId);
+
+            return agent;
+        }
+
+        public async Task<bool> UserHasRentsAsync(string userId)
+        {
+            var userWithRents = await repository.AllReadOnly<House>()
+                .AnyAsync(h => h.RenterId == userId);
+
+            return userWithRents;
+        }
+
+        public async Task<bool> UserWithPhoneNumberExistsAsync(string phoneNumber)
+        {
+            var userWithPhone = await repository.AllReadOnly<Agent>()
+                 .AnyAsync(a => a.PhoneNumber == phoneNumber);
+
+            return userWithPhone;
         }
     }
 }

bfe2a790f2a534797357ba2195aabea9eff0c36b
MVM)7 <mvmanchev07@gmail.com>
2025-08-22 00:09:45 +0300

    BecomeAgent functionality added.

--- HouseRentingSystem7/Controllers/AgentController.cs
+++ HouseRentingSystem7/Controllers/AgentController.cs
@@ -27,9 +27,33 @@ namespace HouseRentingSystem7.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> Become(BecomeAgentFormModel agent)
+        public async Task<IActionResult> Become(BecomeAgentFormModel model)
         {
-            
+            var userId = User.UserId();
+
+            if (await agentService.ExistsByIdAsync(userId))
+            {
+                return BadRequest();
+            }
+
+            if (await agentService.UserWithPhoneNumberExistsAsync(model.PhoneNumber))
+            {
+                ModelState.AddModelError(nameof(model.PhoneNumber), "Phone number already exists.");
+            }
+
+            if (await agentService.UserHasRentsAsync(userId))
+            {
+                ModelState.AddModelError("Error", "You should have no rents to become an agent.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            await agentService.CreateAsync(userId, model.PhoneNumber);
+
+            return RedirectToAction(nameof(HouseController.All), "House");
         }
     }
 }

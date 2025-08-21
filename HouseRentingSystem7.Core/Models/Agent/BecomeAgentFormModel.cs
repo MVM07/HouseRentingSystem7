cb0c2dc8c8cd409546623bd789e6c5a16dc3c736
MVM)7 <mvmanchev07@gmail.com>
2025-08-21 23:50:06 +0300

    BecomeAgentFormModel functionality added.

--- HouseRentingSystem7.Core/Models/Agent/BecomeAgentFormModel.cs
+++ HouseRentingSystem7.Core/Models/Agent/BecomeAgentFormModel.cs
@@ -1,6 +1,13 @@
-﻿namespace HouseRentingSystem7.Core.Models.Agent
+﻿using System.ComponentModel.DataAnnotations;
+using static HouseRentingSystem7.Data.Constants.DataConstants;
+
+namespace HouseRentingSystem7.Core.Models.Agent
 {
     public class BecomeAgentFormModel
     {
+        [Required]
+        [StringLength(AgentPhoneNumberMaxLength, MinimumLength = AgentPhoneNumberMinLength)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; } = string.Empty;
     }
 }

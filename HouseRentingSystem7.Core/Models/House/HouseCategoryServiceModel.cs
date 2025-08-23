4eb9b25cbd60e0e26e7b640799293e92a675bdea
MVM)7 <mvmanchev07@gmail.com>
2025-08-23 22:48:53 +0300

    HouseCategoryServiceModel added

---
+++ HouseRentingSystem7.Core/Models/House/HouseCategoryServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace HouseRentingSystem7.Core.Models.House
+{
+    public class HouseCategoryServiceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}

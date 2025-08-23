335a474827c34dacac8c111df6617be0d006eab8
MVM)7 <mvmanchev07@gmail.com>
2025-08-23 18:26:00 +0300

    Added new message constants

--- HouseRentingSystem7.Core/Constants/MessageConstants.cs
+++ HouseRentingSystem7.Core/Constants/MessageConstants.cs
@@ -5,5 +5,9 @@
         public const string RequiredMessage = "The {0} field is required";
 
         public const string PhoneLengthMessage = "The field {0} must be between {2} and {1} characters long.";
+
+        public const string PhoneExistsMessage = "Phone number already exists.";
+
+        public const string RentsMessage = "You should have no rents to become an agent.";
     }
 }

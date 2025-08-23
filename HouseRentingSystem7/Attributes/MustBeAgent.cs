d1d598e624fb4f412b86840222597a86b53c0552
MVM)7 <mvmanchev07@gmail.com>
2025-08-23 22:44:02 +0300

    MustBeAgent attribute added.

---
+++ HouseRentingSystem7/Attributes/MustBeAgent.cs
@@ -0,0 +1,28 @@
+﻿using HouseRentingSystem7.Core.Contracts;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using HouseRentingSystem7.Controllers;
+
+namespace HouseRentingSystem7.Attributes
+{
+    public class MustBeAgent : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            base.OnActionExecuting(context);
+
+            IAgentService? agentService = context.HttpContext.RequestServices.GetService<IAgentService>();
+
+            if (agentService == null)
+            {
+                context.Result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            if (agentService != null && agentService.ExistsByIdAsync(context.HttpContext.User.UserId()).Result == false)
+            {
+                context.Result = new RedirectToActionResult(nameof(AgentController.Become), "Agent", null);
+            }
+        }
+    }
+}

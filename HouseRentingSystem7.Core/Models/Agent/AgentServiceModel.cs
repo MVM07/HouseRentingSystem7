4c965ed83f20a00954e66bb8d566cd7d652628c8
MVM)7 <mvmanchev07@gmail.com>
2025-09-02 19:43:26 +0300

    AgentServiceModel added.

---
+++ HouseRentingSystem7.Core/Models/Agent/AgentServiceModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HouseRentingSystem7.Core.Models.Agent
+{
+    public class AgentServiceModel
+    {
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
